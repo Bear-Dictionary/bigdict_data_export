9658533d1cb587aca65563d8e065a1b535ae9fee
Tran Giang Long <trangianglong4003@gmail.com>
2021-04-25 18:15:58 +0200

    dynamic export

--- export_dictionary.py
+++ export_dictionary.py
@@ -4,15 +4,15 @@ from pymongo import MongoClient
 
 def export_dictionary(client: MongoClient, name: str, writer: Callable[[str, str, str, str], None]):
     dictionary = client.admin.dictionaries.find_one({'name': name})
-    fromLang = client.admin.languages.find_one({'_id': dictionary['fromLang']})
-    toLang = client.admin.languages.find_one({'_id': dictionary['toLang']})
-    # words = client.admin.words.find({'meanings': SON([('dictionary', dictionary['_id'])])})
+    from_lang = client.admin.languages.find_one({'_id': dictionary['fromLang']})
+    to_lang = client.admin.languages.find_one({'_id': dictionary['toLang']})
     words = client.admin.words.find({'meanings.dictionary': dictionary['_id']})
 
-    print('Dictionary:', fromLang['code'], '->', toLang['code'])
+    print('Dictionary:', from_lang['code'], '->', to_lang['code'])
     count = 0
     for word in words:
         count += 1
         content = next((x for x in word['meanings'] if x['dictionary'] == dictionary["_id"]), None)
-        writer(word['key'], fromLang['code'], toLang['code'], content['htmlString'])
+        writer(word['key'], from_lang['code'], to_lang['code'], content['htmlString'])
+
     print("Export:", count, "words")
--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from csv_writer import CsvWriter
 from export_dictionary import export_dictionary
 from pymongo import MongoClient
+import argparse
 
 if __name__ == '__main__':
-    name = "Từ điển Nga Việt tổng hợp"
-    target = name + '.csv'
+    parser = argparse.ArgumentParser(description='Tool for export data from Bigdict')
+    parser.add_argument('dictionary', metavar='D', help='Dictionary name')
+    parser.add_argument('--address', dest='address', help='MongoDB address',
+                        default='mongodb://root:root@localhost:27017')
+    parser.add_argument('--output', dest='output', help='Output file')
 
-    client = MongoClient("mongodb://root:root@localhost:27017")
-    csvWriter = CsvWriter(target)
+    args = parser.parse_args()
+    mongoAddress = args.address
+    name = args.dictionary
+    output = args.output + '.csv'
+
+    client = MongoClient(mongoAddress)
+    csvWriter = CsvWriter(output)
 
 
-    def writer(word, fromLang, toLang, content):
-        csvWriter.write(word, fromLang, toLang, content)
+    def writer(word: str, from_lang: str, to_lang: str, content: str):
+        csvWriter.write(word, from_lang, to_lang, content)
 
 
     export_dictionary(client=client, name=name, writer=writer)
